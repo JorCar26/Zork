20a2f42a35a8e21fc8c40c9edba25b168ddff6d5
JorCar26 <jordancarswell@knights.ucf.edu>
2021-08-30 15:17:15 -0400

    Zork 1.2 v2
    Includes the switch statment

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -15,35 +15,36 @@ namespace Zork
         private static Commands ToCommand(string commandString)
         {
             Commands command;
+            switch ( commandString)
+            {
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
 
-            if (commandString == "QUIT")
-            {
-                command = Commands.QUIT;
-            }
-            else if (commandString == "Look")
-            {
-                command = Commands.LOOK;
-            }
-            else if (commandString == "NORTH")
-            {
-                command = Commands.NORTH;
-            }
-            else if (commandString == "SOUTH")
-            {
-                command = Commands.SOUTH;
-            }
-            else if (commandString == "EAST")
-            {
-                command = Commands.EAST;
-            }
-            else if (commandString == "WEST")
-            {
-                command = Commands.WEST;
-            }
-            else
-            {
-                command = Commands.UNKNOWN;
-            }
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+
+                default:
+                    command = Commands.UNKNOWN;
+                    break;
+            };
 
             return command;
         }
