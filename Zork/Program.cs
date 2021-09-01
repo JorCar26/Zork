7afa010c64a481ea41adcb29e6a00277d51b7188
JorCar26 <jordancarswell@knights.ucf.edu>
2021-09-01 14:10:51 -0400

    Zork 1.2 Final
    Zork 1.2 step that includes the method on a single line with expression bodied methods.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -12,41 +12,6 @@ namespace Zork
             Commands command = ToCommand(inputString.Trim().ToUpper());
             Console.WriteLine(command);
         }
-        private static Commands ToCommand(string commandString)
-        {
-            Commands command;
-            switch ( commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            };
-
-            return command;
-        }
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
