e65e06e60fb8674f0b8457598d742dd40ef01bdf
JorCar26 <jordancarswell@knights.ucf.edu>
2021-11-20 20:30:09 -0500

    ZorkUnityV2
    Commands dont register but the scrolling view works now as intended

--- ZorkUnity/ZorkInUnity/Assets/Scripts/UnityOutputService.cs
+++ ZorkUnity/ZorkInUnity/Assets/Scripts/UnityOutputService.cs
@@ -1,18 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using Zork;
 using TMPro;
 
 public class UnityOutputService : MonoBehaviour , IOutputService
 {
+    [SerializeField]
+    private int MaxEntries = 60;
+    [SerializeField]
+    private Transform OutputTextContainer;
+    [SerializeField]
+    private Image NewLinePrefab;
+    public UnityOutputService() => Entries = new List<GameObject>();
     public void Clear()
     {
-        throw new System.NotImplementedException();
+        Entries.ForEach(entry => Destroy(entry));
     }
 
     public void Write(string value)
     {
-        
+        ParseAndWriteLine(value);
     }
 
     public void Write(object value)
@@ -22,12 +32,50 @@ public class UnityOutputService : MonoBehaviour , IOutputService
 
     public void WriteLine(string value)
     {
-        OutputText.text = value;
+        ParseAndWriteLine(value);
     }
 
     public void WriteLine(object value)
     {
         WriteLine(value.ToString());
+    }
+
+    private void ParseAndWriteLine(string value)
+    {
+        string[] delimiters = { "\n" };
+
+        var lines = value.Split(delimiters, StringSplitOptions.None);
+        foreach (var line in lines)
+        {
+            if (Entries.Count >= MaxEntries)
+            {
+                var entry = Entries.First();
+                Destroy(entry);
+                Entries.Remove(entry);
+            }
+            if(string.IsNullOrWhiteSpace(line))
+            {
+                WriteNewLine();
+            }
+            else
+            {
+                WriteTextLine(line);
+            }
+        }
+    }
+
+    private void WriteNewLine()
+    {
+        var newLine = GameObject.Instantiate(NewLinePrefab);
+        newLine.transform.SetParent(OutputTextContainer, false);
+        Entries.Add(newLine.gameObject);
+    }
+    private void WriteTextLine(string value)
+    {
+        var textLine = GameObject.Instantiate(TextLinePrefab);
+        textLine.transform.SetParent(OutputTextContainer, false);
+        textLine.text = value;
+        Entries.Add(textLine.gameObject);
     }
 
     void Start()
@@ -40,5 +88,6 @@ public class UnityOutputService : MonoBehaviour , IOutputService
         
     }
     [SerializeField]
-    private TextMeshProUGUI OutputText;
+    private TextMeshProUGUI TextLinePrefab;
+    private readonly List<GameObject> Entries;
 }
